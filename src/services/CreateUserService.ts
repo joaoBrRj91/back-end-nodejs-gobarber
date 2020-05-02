015c4c53d3ec9fdfe4bc69c161f1748ce5cb52fa
João Nascimento <joao1991algoritmo@gmail.com>
2020-05-02 03:15:54 -0300

    Criptografia da senha do usuario

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/User';
 
 interface Request {
@@ -19,13 +20,17 @@ class CreateUserService {
 			throw new Error('Email address already used by another');
 		}
 
+		const hashedPassword = await hash(password, 8);
+
 		const user = usersRepository.create({
 			name,
 			email,
-			password,
+			password: hashedPassword,
 		});
 
 		await usersRepository.save(user);
+
+		delete user.password;
 
 		return user;
 	}
