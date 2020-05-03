ef64f88ded21f2fdc5695188cc03018b5e632b59
João Nascimento <joao1991algoritmo@gmail.com>
2020-05-02 22:39:09 -0300

    Criação da estrutura de session para que o pipeline possa validar as credencias do usuario

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
 import userRouter from './users.routes';
+import sessionRouter from './session.routes';
 
 const routes = Router();
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRouter);
+routes.use('/sessions', sessionRouter);
 
 export default routes;
