2efb43b7eea054fdc3c27c410abc3ebd0a048712
João Nascimento <joao1991algoritmo@gmail.com>
2020-08-01 13:28:22 -0300

    Email em desenvolvimento(EtherealMail)

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -29,11 +29,11 @@ class SendForgotPasswordEmailService {
 			throw new AppError('Usuário não existe.');
 		}
 
-		this.userTokenRepository.generate(user.id);
+		const { token } = await this.userTokenRepository.generate(user.id);
 
-		this.mailProvider.sendMail(
+		await this.mailProvider.sendMail(
 			email,
-			'Pedido de recuperação de senha recebido',
+			`Pedido de recuperação de senha recebido ${token}`,
 		);
 	}
 }
