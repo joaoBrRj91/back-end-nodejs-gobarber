343ae5bb6bf0a0e203f3720fdeb662f8e0c25d3f
João Nascimento <joao1991algoritmo@gmail.com>
2020-08-01 11:46:28 -0300

    Salvando tokens no banco

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,9 +9,17 @@ import AppointmentRepository from '@modules/appointments/infra/typeorm/repositor
 import IUserRepository from '@modules/users/repositories/IUserRepository';
 import UserRepository from '@modules/users/infra/typeorm/repositories/UserRepository';
 
+import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
+import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
+
 container.registerSingleton<IAppointmentRepository>(
 	'AppointmentRepository',
 	AppointmentRepository,
 );
 
 container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
+
+container.registerSingleton<IUserTokenRepository>(
+	'UserTokenRepository',
+	UserTokenRepository,
+);
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import userRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionRouter from '@modules/users/infra/http/routes/session.routes';
+import passwordRouter from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', userRouter);
 routes.use('/sessions', sessionRouter);
+routes.use('/password', passwordRouter);
 
 export default routes;
