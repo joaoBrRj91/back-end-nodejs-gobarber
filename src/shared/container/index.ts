c5059293d8f457c4595511f885374fe645fee06e
João Nascimento <joao1991algoritmo@gmail.com>
2020-09-08 17:40:53 -0300

    Enviando notificações

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -12,6 +12,9 @@ import UserRepository from '@modules/users/infra/typeorm/repositories/UserReposi
 import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
 import UserTokenRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
 
+import INotificationsRepository from '@modules/notifications/repositories/INotificationsRepository';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
+
 container.registerSingleton<IAppointmentRepository>(
 	'AppointmentRepository',
 	AppointmentRepository,
@@ -22,4 +25,9 @@ container.registerSingleton<IUserRepository>('UserRepository', UserRepository);
 container.registerSingleton<IUserTokenRepository>(
 	'UserTokenRepository',
 	UserTokenRepository,
+);
+
+container.registerSingleton<INotificationsRepository>(
+	'NotificationsRepository',
+	NotificationsRepository,
 );
